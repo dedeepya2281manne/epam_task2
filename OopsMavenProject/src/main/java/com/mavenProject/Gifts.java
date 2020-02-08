bda59be9c925b500114b59dbe524bc5d9041c845
dedeepya <dedeepyamanne02@gmail.com>
2020-02-08 19:10:46 +0530

    Maven Project based on Oops Concept

---
+++ OopsMavenProject/src/main/java/com/mavenProject/Gifts.java
@@ -0,0 +1,17 @@
+package com.mavenProject;
+
+import java.util.ArrayList;
+
+public class Gifts {
+	public int weight;
+	public int cost;
+	public static ArrayList<Integer> wgt = new ArrayList<Integer>();
+	public int getWeight(){
+		int totalWeight = 0;
+		for(int i = 0;i < wgt.size(); i++){
+			totalWeight += wgt.get(i);
+		}
+		return totalWeight;
+	}
+	
+}
